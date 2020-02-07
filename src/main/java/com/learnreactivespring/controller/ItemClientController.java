07ba9d62ca3c720266354ea73c83e572da8ec43f
Julian Cabrera <julian.cabreras@gmail.com>
2020-02-06 21:57:26 -0600

    Section 15 Build Non Blocing Client using WebClilent

--- src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -1,10 +1,11 @@
 package com.learnreactivespring.controller;
 
 import com.learnreactivespring.domain.Item;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class ItemClientController {
@@ -12,20 +13,80 @@ public class ItemClientController {
     WebClient webClient = WebClient.create("http://localhost:8080");
 
     @GetMapping("/client/retrieve")
-    public Flux<Item> getAllItemUsingRetrieve(){
+    public Flux<Item> getAllItemUsingRetrieve() {
 
         return webClient.get().uri("/v1/items")
                 .retrieve()
                 .bodyToFlux(Item.class)
                 .log("items in Client Project retrieve: ");
     }
+
     @GetMapping("/client/exchange")
-    public Flux<Item> getAllItemUsingExchange(){
+    public Flux<Item> getAllItemUsingExchange() {
 
         return webClient.get().uri("/v1/items")
                 .exchange()
                 .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class))
                 .log("items in Client Project exchange: ");
     }
+
+    @GetMapping("/client/retrieve/singleItem")
+    public Mono<Item> getOneItemUsingRetrieve() {
+
+        String id = "ABC";
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Items in CLinet Project retrieve single item: ");
+    }
+
+    @GetMapping("/client/exchange/singleItem")
+    public Mono<Item> getOneItemUsingExchange() {
+
+        String id = "ABC";
+
+        return webClient.get().uri("/v1/items/{id}", id)
+                .exchange()
+                .flatMap(clientResponse -> clientResponse.bodyToMono(Item.class))
+                .log("Items in CLinet Project retrieve single item: ");
+    }
+
+    @PostMapping("/client/createItem")
+    public Mono<Item> createItem(@RequestBody Item item) {
+
+        Mono<Item> itemMono = Mono.just(item);
+
+        return webClient.post().uri("/v1/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemMono, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Created item is: ");
+
+    }
+
+    @DeleteMapping("client/updateItem/{id}")
+    public Mono<Void> deleteItem(@PathVariable String id) {
+        return webClient.delete().uri("/v1/items/{id}", id)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .log("Delete Item is");
+    }
+
+
+    @PutMapping("client/updateItem/{id}")
+    public Mono<Item> updateItem(@PathVariable String id, @RequestBody Item item) {
+
+        Mono<Item> itemMono = Mono.just(item);
+
+        webClient.put().uri("/v1/items/{id}", id)
+                .body(itemMono, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Updated item is: ");
+
+    }
+
 
 }
